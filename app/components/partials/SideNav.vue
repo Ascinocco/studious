f9fdf6d4a175ee08169b4ddaa4f24dec7612ebca
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-28 00:00:28 -0400

    trying to get semantic ui to work

--- app/components/partials/SideNav.vue
+++ app/components/partials/SideNav.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1>Side Nav</h1>
+    <div class="ui vertical inverted sidebar menu">
+        <div class="item">
+            <div class="header">App</div>
+            <div class="menu">
+                <router-link to="/" class="item">Dashboard</router-link>
+                <router-link to="/classes" class="item">Classes</router-link>
+                <router-link to="/homework" class="item">Homework</router-link>
+                <router-link to="/settings" class="item">Settings</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
